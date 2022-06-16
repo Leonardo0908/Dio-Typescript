c4a16a3f27f9fbd14a7c74a416c116c9e37ccbb2
Leonardo E S Rodrigues <leonardo.elias0908@gmail.com>
2022-06-16 15:40:40 -0300

    adicionando exemplos e modificando desafio2

--- src/desafios/desafio2.js
+++ src/desafios/desafio2.js
@@ -3,7 +3,7 @@
 let pessoa1 = {};
 pessoa1.nome = "maria";
 pessoa1.idade = 29;
-pessoa1.profissao = "atriz"
+pessoa1.profissao = "atriz";
 
 let pessoa2 = {}
 pessoa2.nome = "roberto";
@@ -17,7 +17,7 @@ let pessoa3 = {
 };
 
 let pessoa4 = {
-    nome = "carlos",
-    idade = 19,
-    profissao = "padeiro"
+    nome: "carlos",
+    idade: 19,
+    profissao: "padeiro"
 }
